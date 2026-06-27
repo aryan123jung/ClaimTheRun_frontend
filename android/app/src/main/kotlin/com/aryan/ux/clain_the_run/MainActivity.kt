bb632f92287bc52b2eb6dc294fac0b8d6e748ef7
aryan123jung <aryanjungchhetri@gmail.com>
2026-06-27 16:12:33 +0545

    development

---
+++ android/app/src/main/kotlin/com/aryan/ux/clain_the_run/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.aryan.ux.clain_the_run
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/app/app.dart
@@ -0,0 +1,14 @@
+import 'package:clain_the_run/features/splash/presentation/pages/splashscreen.dart';
+import 'package:flutter/material.dart';
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: const SplashScreen(),
+    );
+  }
+}
